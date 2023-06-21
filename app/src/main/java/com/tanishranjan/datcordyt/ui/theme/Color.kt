fddd5f3c1019c46d23a8c00d7e56c1eaaa92b583
Tanish-Ranjan <tanishranjan4@gmail.com>
2023-06-22 01:03:58 +0530

    Initial commit

---
+++ app/src/main/java/com/tanishranjan/datcordyt/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.tanishranjan.datcordyt.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF5865F2)
+val EngravedSurface = Color(0xFF1E1F23)
+val Surface = Color(0xFF40434A)
+val Background = Color(0xFF323338)
+val Highlight = Color(0xFF0EA6EF)
